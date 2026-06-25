6c2cacb0b2dc93c77ba961fbcf5e18f24f0541eb
Siam <siam70344@gmail.com>
2026-06-25 23:18:03 +0600

    3rd

--- models/Payment.js
+++ models/Payment.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// Define the Payment schema
+// Define the Payment schem
 const paymentSchema = new mongoose.Schema({
   client_email: { type: String, required: true },
   freelancer_email: { type: String, required: true },
