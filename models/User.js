37ea76fec4e655662fc1ec8132c969f5418eff62
Siam <siam70344@gmail.com>
2026-06-25 23:49:52 +0600

    ok

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+// Define the User schema
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
